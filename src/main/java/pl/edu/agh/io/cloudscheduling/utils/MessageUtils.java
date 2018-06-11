0d45f1f8fceedd16842a103a696cf778948b8196
somas3k <sikorka1901@gmail.com>
2018-06-11 09:12:50 +0200

    Simple change with serialization

--- src/main/java/pl/edu/agh/io/cloudscheduling/utils/MessageUtils.java
+++ src/main/java/pl/edu/agh/io/cloudscheduling/utils/MessageUtils.java
@@ -1,29 +1,33 @@
 package pl.edu.agh.io.cloudscheduling.utils;
 
+import org.apache.commons.lang3.SerializationUtils;
+
 import java.io.*;
 
 public class MessageUtils {
 
-    public static Object deserializeMessage (byte[] body) throws ClassNotFoundException {
-        try {
-            ObjectInput oin = new ObjectInputStream(new ByteArrayInputStream(body));
-            return oin.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public static Serializable deserializeMessage (byte[] body) throws ClassNotFoundException {
+        return SerializationUtils.deserialize(body);
+//        try {
+//            ObjectInput oin = new ObjectInputStream(new ByteArrayInputStream(body));
+//            return oin.readObject();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
     }
 
-    public static byte[] serializeMessage(Object o){
-        try{
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(bos);
-            oos.writeObject(o);
-            return bos.toByteArray();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-            return null;
-        }
+    public static byte[] serializeMessage(Serializable o){
+        return SerializationUtils.serialize(o);
+//        try{
+//            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//            ObjectOutputStream oos = new ObjectOutputStream(bos);
+//            oos.writeObject(o);
+//            return bos.toByteArray();
+//        }
+//        catch (IOException e){
+//            e.printStackTrace();
+//            return null;
+//        }
     }
 }
